2880ab1fea7eb48a77ce1f33188444c158f87377
Ajay Singh Thakur <ajaysinghthakur735@gmail.com>
2024-08-26 22:40:46 +0530

    Update Main.java
    Initial Commit

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 import java.time.LocalDateTime;
+import java.util.Date;
 
 public class Main{
 
     public static void main(String[] args){
         System.out.println("Hiiii");
         System.out.println(LocalDateTime.now());
+        System.out.println(new Date());
    
     }
 }
