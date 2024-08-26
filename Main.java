9d743deb7477bdcbf30454889679bce67947e0a0
Ajay Singh Thakur <ajaysinghthakur735@gmail.com>
2024-08-26 22:38:32 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("Hiiii");
+        System.out.println(LocalDateTime.now());
+   
+    }
+}
